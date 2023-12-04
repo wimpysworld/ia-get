fcfdaa6ef0facc9016e8e19eca558087b20ae043
Martin Wimpress <martin@wimpress.org>
2023-12-04 11:08:35 +0000

    refactor: parse the XML and print every field
    Used Unfold.ai:
    
    This program uses the serde and serde_xml_rs crates for XML deserialization. It defines two struct types: `File` to represent each file entry, and `Files` to represent the root element containing multiple files. The program opens the XML file, parses it into the `Files` struct, and then iterates over each file to print its fields. Optional fields are handled using `Option` types.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::io::Write;
 use std::path::Path;
 
 #[derive(Deserialize, Debug)]
-struct XmlRoot {
+struct Files {
     #[serde(rename = "file")]
     files: Vec<File>,
 }
@@ -62,29 +62,60 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let response = reqwest::get(xml_url).await?.text().await?;
 
     // 2. Parse XML
-    let parsed_xml: XmlRoot = from_str(&response)?;
-
-    // 3. Extract file URLs
-    let file_urls: Vec<String> = parsed_xml
-        .files
-        .into_iter()
-        .map(|f| f.name)
-        .collect();
+    let files: Files = from_str(&response)?;
 
     // Get the base URL from the XML URL
     let base_url = reqwest::Url::parse(xml_url)?;
 
-    // 4. Download each file
-    for url in file_urls {
+    // Iterate over the files and print every field
+    for file in files.files {
+        println!("------------------");
+        println!("Name: {}", file.name);
+        println!("Source: {}", file.source);
+        if let Some(mtime) = file.mtime {
+            println!("MTime: {}", mtime);
+        }
+        if let Some(size) = file.size {
+            println!("Size: {}", size);
+        }
+        if let Some(format) = file.format {
+            println!("Format: {}", format);
+        }
+        if let Some(rotation) = file.rotation {
+            println!("Rotation: {}", rotation);
+        }
+        if let Some(md5) = file.md5 {
+            println!("MD5: {}", md5);
+        }
+        if let Some(crc32) = file.crc32 {
+            println!("CRC32: {}", crc32);
+        }
+        if let Some(sha1) = file.sha1 {
+            println!("SHA1: {}", sha1);
+        }
+        if let Some(btih) = file.btih {
+            println!("BTIH: {}", btih);
+        }
+        if let Some(summation) = file.summation {
+            println!("Summation: {}", summation);
+        }
+        if let Some(original) = file.original {
+            println!("Original: {}", original);
+        }
+        if let Some(old_version) = file.old_version {
+            println!("Old Version: {}", old_version);
+        }
+
+        // 4. Download each file
         let mut absolute_url = base_url.clone();
 
         // If the URL is relative, join it with the base_url to make it absolute
-        match absolute_url.join(&url) {
+        match absolute_url.join(&file.name) {
             Ok(joined_url) => absolute_url = joined_url,
             Err(_) => {} // If it's an error, it might already be an absolute URL. Ignore.
         }
 
-        let filename = url.split('/').last().unwrap_or("unknown_file");
+        let filename = file.name.split('/').last().unwrap_or("unknown_file");
 
         // Check if the file already exists
         if Path::new(filename).exists() {
@@ -93,10 +124,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
 
         let mut response = client.get(absolute_url).send().await?;
-        let mut file = std::fs::File::create(filename)?;
+        let mut download = std::fs::File::create(filename)?;
 
         while let Some(chunk) = response.chunk().await? {
-            file.write_all(&chunk)?;
+            download.write_all(&chunk)?;
         }
 
         println!("Downloaded: {}", filename);
